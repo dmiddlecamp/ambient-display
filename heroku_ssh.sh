5d4085d0c03103cca3b9aa7131b9c00128da92e3
David Middlecamp <david@spark.io>
2014-08-07 15:25:05 -0500

    init

---
+++ heroku_ssh.sh
@@ -0,0 +1,12 @@
+
+ssh-add -D
+ssh-add ~/.ssh/heroku_id_rsa
+#git subtree push --prefix bus_publisher heroku master
+
+git init
+git add .
+git commit -m"init"
+git remote add heroku git@heroku.com:bus-publisher.git
+git push heroku master
+
+
